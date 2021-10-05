ab4f284613436f257036d5f720d9b26288abe441
ahyun922 <33361135+ahyun922@users.noreply.github.com>
2021-10-05 16:59:33 -0700

    Add files via upload

---
+++ p5-mario-game-main/p5-mario-game-main/sketch.js
@@ -0,0 +1,63 @@
+
+const numCoins = 100
+
+const coins = Array.from({ length: numCoins}, (el, i) => {
+    return new Coin({ x: 20 * i, y: 100 }, 20)
+})
+const heroMario = new Mario({x: 200, y: 280}, 120)
+
+function preload(){
+    objectsImg = loadImage('assets/items-objects.png')
+    marioImg = loadImage('assets/mario-use.png')
+}
+
+function setup(){
+    createCanvas(1000, 600)
+    background(95, 138, 245)
+    frameRate(15)
+
+    //assign images
+    heroMario.images = loadMarioImages(marioImg)
+    coins.forEach( coin => coin.images = loadCoinImages(objectsImg))
+    
+}
+
+function draw(){
+    background(95, 138, 245)
+    fill(110, 59, 21)
+    //this is the ground
+    rect(0, 400, 1000, 200)
+
+    //draw the coins
+    coins.forEach(coin => {
+        coin.update()
+        coin.render()
+    })
+
+    //draw mario
+    checkKeys()
+    heroMario.render()
+    heroMario.update()
+
+}
+
+function checkKeys(){
+    if(keyIsDown(UP_ARROW)){
+        heroMario.jump()
+        return
+    } else if(keyIsDown(LEFT_ARROW)){
+        heroMario.runLeft()
+        return
+    } else if(keyIsDown(RIGHT_ARROW)){
+        heroMario.runRight()
+        return
+    }
+
+}
+
+
+function keyReleased(){
+    if(keyCode === UP_ARROW){
+    heroMario.clearJump()
+    }
+}
